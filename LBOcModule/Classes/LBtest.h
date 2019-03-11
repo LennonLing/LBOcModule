b9d56b01f195d30d17d5b7a2ec1d79cb1eac1afa
lingbin <bin.ling.2018@gmail.com>
2019-03-11 15:48:15 +0800

    第一次跳

---
+++ LBOcModule/Classes/LBtest.h
@@ -0,0 +1,19 @@
+//
+//  LBtest.h
+//  1
+//
+//  Created by 凌斌 on 2019/3/11.
+//  Copyright © 2019 lingbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LBtest : NSObject
+
++ (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LBOcModule/Classes/LBtest.m
@@ -0,0 +1,17 @@
+//
+//  LBtest.m
+//  1
+//
+//  Created by 凌斌 on 2019/3/11.
+//  Copyright © 2019 lingbin. All rights reserved.
+//
+
+#import "LBtest.h"
+
+@implementation LBtest
+
++ (void)log {
+    NSLog(@"log");
+}
+
+@end
